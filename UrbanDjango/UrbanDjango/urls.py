1731aab3c4899827821958e2570da2b9529dad75
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-11-05 17:02:41 +0300

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from task3.views import index, store, basket
+from task4.views import index, store, basket
 from django.views.generic import TemplateView
 
 
